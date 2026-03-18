24f2e2abe29657ab59bf3d6ce1679605a3f3270d
Dan Dumitru <dandumitru27@gmail.com>
2026-03-18 12:30:06 +0200

    Evaluate notebook; extracted common code in moderate.py

---
+++ moderate.py
@@ -0,0 +1,67 @@
+import json
+import os
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+
+
+def initialize_agent():
+    use_databricks = False
+
+    if use_databricks:
+        os.environ["OPENAI_API_KEY"] = dbutils.secrets.get(
+            scope="MainSecretScope", key="OPENAI_API_KEY"
+        )
+        os.environ["GOOGLE_API_KEY"] = dbutils.secrets.get(
+            scope="MainSecretScope", key="GOOGLE_API_KEY"
+        )
+    else:
+        load_dotenv()
+
+    provider = "google_genai"  # "google_genai" / "openai"
+
+    if provider == "google_genai":
+        model = "google_genai:gemini-3-flash-preview"
+    else:
+        model = "gpt-5-mini"
+
+    return create_agent(model)
+
+
+def moderate_profile_description(ad_text, agent):
+
+    prompt = f"""
+        Esti un moderator de continut profesionist.
+
+        Analizeaza urmatorul text al unui anunt adaugat de un mester pe o platforma de servicii si determina daca este:
+        - Gibberish (text fara sens)
+        - Spam (text care incearca sa promoveze ceva sau sa atraga atentia in mod nejustificat)
+        - Inappropriate (text care contine limbaj ofensator, discriminare, sau alte elemente nepotrivite)
+        - Language (limba in care este scris textul)
+        - Valid (daca nu este nici gibberish, nici spam, nici inappropriate, iar limba este fie romana, maghiara, sau engleza)
+
+        "{ad_text}"
+
+        Returneaza DOAR JSON valid in acest format:
+
+        {{
+        "is_valid": true/false,
+        "is_gibberish": true/false,
+        "is_spam": true/false,
+        "is_inappropriate": true/false,
+        "language": "detected language",
+        "confidence": 0.0-1.0,
+        "reason": "short explanation"
+        }}
+        """
+
+    response = agent.invoke({"messages": [{"role": "user", "content": prompt}]})
+
+    content = response["messages"][-1].content
+
+    if isinstance(content, list):
+        text = content[0]["text"]
+    else:
+        text = content
+
+    return json.loads(text)
